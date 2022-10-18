7418b3be2e95a7cc49abfdb83f6154ccc6e7f090
Colin Cameron <ccameron2020@gmail.com>
2022-10-18 16:07:15 +0100

    update

---
+++ src/containers/NewsContainer.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import NewsStoryList from "../components/NewsStoryList";
+import StoryListItem from "../components/StoryListItem";
+
+
+const NewsContainer = (() => {
+  
+    const [stories, setStories] = useState([])
+        const [contents, setContents] = useState ([])
+
+        useEffect(() => {
+          fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
+          .then(res => res.json())
+          .then(data => setStories(data))
+
+
+
+
+
+
+
+            console.log(stories)
+            
+       },[])
+
+    
+     
+     
+        const storyPromises = stories.map((story) => {
+        return fetch(story).then(res => res.json())})
+        Promise.all(storyPromises).then((data) => {
+        setContents(data)})
+     
+     
+     
+
+         
+
+
+    return(
+        <>
+        <NewsStoryList stories={stories}/>
+        </>
+    )
+
+})
+export default NewsContainer
+
+
+// https://hacker-news.firebaseio.com/v0/topstories.json
+// https://hacker-news.firebaseio.com/v0/item/{storyId}.json 
